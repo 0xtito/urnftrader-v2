87dce01a7ad1922a7623b33becbfb9fc20c1b925
0xtito <welovethepenguins@gmail.com>
2023-02-28 12:58:43 -0500

    get collection w/ custom data

--- src/pages/api/collections/[collection].ts
+++ src/pages/api/collections/[collection].ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Alchemy, Network, Nft, NftMetadataBatchToken } from "alchemy-sdk";
 import { MarketplaceInfo, NFT, Traits, Collection } from "../../../interfaces";
+import { paths } from "@reservoir0x/reservoir-sdk";
 
 import { Order, OrdersResponse } from "../../../interfaces";
 
@@ -46,16 +47,43 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
 
     const allNftsMetadata = await alchemy.nft.getNftMetadataBatch(tokens);
 
-    // Have to set up return statement with proper types
-    // const nfts: NFT[] = listedNfts.orders.map((nft, i) => {
-    //   let nftMetadata = allNftsMetadata[i];
-    //   return {
-    //     name: nftMetadata.title
-    //   }
-    // });
+    // An array of nfts listed from a specific collection +
+    // with all necessary metadata
+    const nfts: NFT[] = allNftsMetadata.map((nft, i) => {
+      // nft metadata from alchemy
+      const nftListing = listedNfts.orders.find((order) => {
+        return order.tokenSetId.split(":")[2] == nft.tokenId;
+      });
+      // const nftListing = listedNfts.orders[i];
+      return {
+        name: nft.title,
+        image: nft.rawMetadata?.image,
+        description: nft.rawMetadata?.description,
+        traits: nft.rawMetadata?.attributes,
+        tokenId: nft.tokenId,
+        price: nftListing?.price.amount.decimal,
+        marketplace: {
+          name: nftListing?.source.name as string | undefined,
+          icon: nftListing?.source.icon as string | undefined,
+          assetPage: nftListing?.source.url as string | undefined,
+        },
+        maker: nftListing?.maker,
+      };
+    });
 
-    return allNftsMetadata
-      ? res.status(200).json(allNftsMetadata)
+    const collection: Collection = {
+      name: allNftsMetadata[0].contract.openSea?.collectionName as string,
+      logo: allNftsMetadata[0].contract.openSea?.imageUrl,
+      collectionDescription: allNftsMetadata[0].contract.openSea?.description,
+      floorPrice: allNftsMetadata[0].contract.openSea?.floorPrice,
+      verified: allNftsMetadata[0].contract.openSea?.safelistRequestStatus,
+      externalUrl: allNftsMetadata[0].contract.openSea?.externalUrl,
+      twitterHandle: allNftsMetadata[0].contract.openSea?.twitterUsername,
+      discord: allNftsMetadata[0].contract.openSea?.discordUrl,
+      nfts,
+    };
+    return nfts
+      ? res.status(200).json(collection)
       : res.status(500).json({ message: "data not found" });
   } catch (err) {
     res.status(500).json(err);
