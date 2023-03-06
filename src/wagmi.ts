b267053ebfdc31fe7425db304ce15ff5d5c377d4
0xtito <welovethepenguins@gmail.com>
2023-03-06 14:42:26 -0500

    added ZeroDev

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,16 +1,31 @@
 import { configureChains, createClient } from "wagmi";
-import { goerli, mainnet } from "wagmi/chains";
+import { goerli, mainnet, polygonMumbai } from "wagmi/chains";
 import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
 import { InjectedConnector } from "wagmi/connectors/injected";
 import { MetaMaskConnector } from "wagmi/connectors/metaMask";
 import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+// import {
+//   GoogleSocialWalletConnector,
+//   GithubSocialWalletConnector,
+//   DiscordSocialWalletConnector,
+//   TwitterSocialWalletConnector,
+// } from "@zerodevapp/wagmi";
+// import { getPrivateKeyOwner } from "@zerodevapp/sdk";
+// import { ZeroDevConnector } from "@zerodevapp/wagmi";
 
 import { publicProvider } from "wagmi/providers/public";
 
 const { chains, provider, webSocketProvider } = configureChains(
-  [mainnet, ...(process.env.NODE_ENV === "development" ? [goerli] : [])],
+  [
+    mainnet,
+    ...(process.env.NODE_ENV === "development" ? [polygonMumbai, goerli] : []),
+  ],
   [publicProvider()]
 );
+
+const options = {
+  options: { projectId: "b5486fa4-e3d9-450b-8428-646e757c10f6" },
+};
 
 export const client = createClient({
   autoConnect: true,
@@ -19,7 +34,7 @@ export const client = createClient({
     new CoinbaseWalletConnector({
       chains,
       options: {
-        appName: "wagmi",
+        appName: "UrNFTrader",
       },
     }),
     new WalletConnectConnector({
@@ -28,6 +43,32 @@ export const client = createClient({
         qrcode: true,
       },
     }),
+    // new ZeroDevConnector({
+    //   chains,
+    //   options: {
+    //     projectId: "UrNFTrader",
+    //   },
+    // }),
+    // new GoogleSocialWalletConnector(options),
+    // new GithubSocialWalletConnector(options),
+    // new GithubSocialWalletConnector({
+    //   chains,
+    //   options: {
+    //     projectId: "UrNFTrader",
+    //   },
+    // }),
+    // new DiscordSocialWalletConnector({
+    //   chains,
+    //   options: {
+    //     projectId: "UrNFTrader",
+    //   },
+    // }),
+    // new TwitterSocialWalletConnector({
+    //   chains,
+    //   options: {
+    //     projectId: "UrNFTrader",
+    //   },
+    // }),
   ],
   provider,
   webSocketProvider,
