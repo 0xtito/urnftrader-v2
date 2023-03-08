d98b7790da5e7946596899359230646f3b315b2f
0xtito <welovethepenguins@gmail.com>
2023-03-08 00:08:16 -0500

    add libraries/config nextjs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["@zerodevapp", "@web3"],
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/app/home",
+        permanent: true,
+      },
+      {
+        source: "/app",
+        destination: "/app/home",
+        permanent: true,
+      },
+    ];
+  },
 };
